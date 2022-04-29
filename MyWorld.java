577c86d338071a3905b3acc4438b7acee9ce5cf8
Chloe Hoang <335731170@gapps.yrdsb.ca>
2022-04-29 14:43:02 -0400

    Move Elephant

--- MyWorld.java
+++ MyWorld.java
@@ -17,5 +17,25 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        animal();
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void animal()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,300, 200);
+        elephant.setLocation(50,200);
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }
